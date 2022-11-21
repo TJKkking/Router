84263c976af6aa2c8307f1e4e2ae4c044197dd4b
taojikun <2510399607@qq.com>
2022-11-21 05:56:47 -0800

    add listen

--- router.go
+++ router.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"time"
@@ -24,7 +25,7 @@ type Router struct {
 
 type TableEntry struct {
 	Refused  bool
-	Route    []int
+	Route    []string
 	Distance int
 }
 
@@ -34,6 +35,13 @@ func (r *Router) routerInit(id string, port int, nbs []int) {
 	r.Neighbors = nbs
 	r.RoutingTable = make(map[string]TableEntry)
 
+	// for _, v := range r.Neighbors {
+	// 	r.RoutingTable[strconv.Itoa(v)] = TableEntry{
+	// 		Refused:  false,
+	// 		Route:    []string{},
+	// 		Distance: 1,
+	// 	}
+	// }
 }
 
 func (r *Router) listen() {
@@ -46,6 +54,7 @@ func (r *Router) listen() {
 		buf := make([]byte, 4096)
 		_, srcAddr, err := conn.ReadFromUDP(buf)
 		checkError(err)
+		log.Printf("[receive]table from port: %d", srcAddr.Port)
 
 		data := bytes.NewBuffer(buf)
 		decoder := gob.NewDecoder(data)
@@ -53,9 +62,57 @@ func (r *Router) listen() {
 		err = decoder.Decode(&srcRouter) //input must be memory address
 		checkError(err)
 
-		srcTable = srcRouter.RoutingTable
+		srcTable := srcRouter.RoutingTable
+		for k, v := range srcTable {
+			if r.RoutingTable[k].Refused { // destination is refused todo
+				continue
+			}
+			if _, ok := r.RoutingTable[k]; ok { //already existed
+				var tobeUpdate bool = false
+				if r.RoutingTable[k].Route[0] == v.Route[0] { // 下一跳相同
+					if !r.RefuseCheck(v.Route) { // 不包含refused node 更新
+						tobeUpdate = true
+						log.Printf("[update][router-" + r.ID + ", from-" + srcRouter.ID + ", des-" + k + "]node already existed, same next hop")
+					}
 
+				} else if r.RoutingTable[k].Distance > v.Distance+1 { // 下一跳不同且path更短
+					if !r.RefuseCheck(v.Route) { // 不包含refused node 更新
+						tobeUpdate = true
+						log.Printf("[update][router-" + r.ID + ", from-" + srcRouter.ID + ", des-" + k + "]node already existed, different next hop")
+					}
+				}
+				if tobeUpdate { // update routing table
+					r.RoutingTable[k] = TableEntry{ //todo: 不可达的情况是否需要特殊考虑
+						Refused:  false,
+						Distance: v.Distance + 1,
+						Route:    append([]string{srcRouter.ID}, v.Route...),
+					}
+				}
+			} else { // new table entry
+				if !r.RefuseCheck(v.Route) { // 新节点且path不包含refused node
+					r.RoutingTable[k] = TableEntry{
+						Distance: v.Distance + 1,
+						Refused:  false,
+						Route:    append([]string{srcRouter.ID}, v.Route...),
+					}
+					log.Printf("[update][router-" + r.ID + ", from-" + srcRouter.ID + ", des-" + k + "]new node")
+				}
+			}
+		}
+		// todo: save update info to log
+		// todo: loop?
 	}
+}
+
+func (r *Router) RefuseCheck(route []string) bool {
+	var isRefused bool = false
+	for _, y := range route {
+		if r.RoutingTable[y].Refused == true { //Refused
+			isRefused = true
+			break
+		}
+	}
+	return isRefused
 }
 
 func (r *Router) broadcast() {
